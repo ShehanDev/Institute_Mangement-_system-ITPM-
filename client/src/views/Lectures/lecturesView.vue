fa5772223811845f517ef509d8feaf5579a61028
Chathurani <dhkchathu@gmail.com>
2022-05-05 01:26:02 +0530

    Update breadcrumbs

--- client/src/views/Lectures/lecturesView.vue
+++ client/src/views/Lectures/lecturesView.vue
@@ -13,7 +13,6 @@
                <v-toolbar-title class="title black--text ml-2">
                 Home  >  Lecturers<span class="caption">
                   <br>
-                  
                 </span>
               </v-toolbar-title>
               <v-spacer></v-spacer>
