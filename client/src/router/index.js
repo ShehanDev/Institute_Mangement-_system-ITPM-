01d6b7d16f3320c706c5230f6abd9ed22efc10f3
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-05-06 14:56:12 -0700

    Added routing path for AddedStudent file

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,13 +3,14 @@ import VueRouter from 'vue-router'
 import dashboard  from '../views/Dashboard/dashboardView.vue'
 import student  from '../views/Students/studentView.vue'
 import addstudent  from '../views/Students/StudentsAdd.vue'
+import addedstudent  from '../views/Students/AddedStudent.vue'
 // import courses  from '../views/Courses/courses.vue'
 // import addcourse  from '../views/Courses/CourseAdd.vue'
 // import booksveiw  from '../views/Library/BooksView.vue'
 // import addbook  from '../views/Library/BooksAdd.vue'
 // import addlectures  from '../views/Lectures/LectureAdd.vue'
 // import lectures  from '../views/Lectures/lecturesView.vue'
-// import addedstudent  from '../views/Students/AddedStudent.vue'
+
 // import addedcourse  from '../views/Courses/AddedCourse.vue'
 // import addedlecture from '../views/Lectures/AddedLecture.vue'
 // import addedbook from '../views/Library/AddedBook.vue'
@@ -30,6 +31,20 @@ const routes = [
     name: 'dashboard',
     component: dashboard
   },
+
+  {
+    path: '/addedstudent',
+    name: 'addedstudent',
+    component: addedstudent
+  },
+
+
+
+
+
+
+
+
   // {
   //   path: '/courses',
   //   name: 'courses',
@@ -77,12 +92,7 @@ const routes = [
   //   component: addlectures
 
   // },
-  // {
-  //   path: '/addedstudent',
-  //   name: 'addedstudent',
-  //   component: addedstudent
-  // },
-
+  
   // {
   //   path: '/addedcourse',
   //   name: 'addedcourse',
