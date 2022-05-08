653f87aa3ec195f274a8bfae7357ce77544e3982
Shehan Mallawaarchchi <shehan96mts@gmail.com>
2022-05-08 21:51:19 +0530

    coures routes updated

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -20,83 +20,34 @@ Vue.use(VueRouter)
 const routes = [
   
 
-  // {
-  //   path: '/student',
-  //   name: 'student',
-  //   component: student
-  // },
+
   {
     path: '/',
     name: 'dashboard',
     component: dashboard
   },
-  // {
-  //   path: '/courses',
-  //   name: 'courses',
-  //   component: courses
-  // },
-  // {
-  //   path: '/addstudent',
-  //   name: 'addstudent',
-  //   component: addstudent
-  // },
-  // {
-  //   path: '/addcourse',
-  //   name: 'addcourse',
-  //   component: addcourse
+  {
+    path: '/courses',
+    name: 'courses',
+    component: courses
+  },
+
+  {
+    path: '/addcourse',
+    name: 'addcourse',
+    component: addcourse
     
-  // },
-  // {
-  //   path: '/booksveiw',
-  //   name: 'booksveiw',
-  //   component: booksveiw
-    
-  // },
-  // {
-  //   path: '/addbook',
-  //   name: 'addbook',
-  //   component: addbook
-    
-  // },
-  // {
+  },
+  {
+    path: '/coursesReport',
+    name: 'coursesReport',
+    component: coursesReport
+  }
+  
 
-  //   path: '/lectures',
-  //   name: 'lectures',
-  //   component: lectures
 
-  // },
-  // {
+ 
 
-  //   path: '/addlecture',
-
-  //   name: 'lecture',
-
-  //   component: addlectures
-
-  // },
-  // {
-  //   path: '/addedstudent',
-  //   name: 'addedstudent',
-  //   component: addedstudent
-  // },
-
-  // {
-  //   path: '/addedcourse',
-  //   name: 'addedcourse',
-  //   component: addedcourse
-  // },
-
-  // {
-  //   path: '/addedlecture',
-  //   name: 'addedlecture',
-  //   component: addedlecture
-  // },
-
-  // {
-  //   path: '/addedbook',
-  //   name: 'addedbook',
-  //   component: addedbook
-  // },
 ]
 
 const router = new VueRouter({
--- client/src/views/Courses/coursesReport.vue
+++ client/src/views/Courses/coursesReport.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-   <v-toolbar-title class="title black--text ml-1">
+     <v-toolbar-title class="title black--text ml-1">
       ALL COURSES<span class="caption">
         <br />
       </span>
@@ -12,11 +12,12 @@
      <container>
        <span class="title black--text ml-2">Home > Courses </span>
      </container></v-col><v-spacer></v-spacer><v-spacer></v-spacer><v-spacer></v-spacer><v-col>
-      <container><v-btn color="primary" to="coursesReport">Genarate Pdf</v-btn></container></v-col>
+      <container><v-btn color="primary" @click="downloadPDF()">Generate PDF</v-btn></container></v-col>
         </v-row>
       </container>
     </v-toolbar-title>
     <br />
+
     <v-alert
       border="left"
       close-text="Close Alert"
@@ -28,7 +29,7 @@
       {{ this.message }}
     </v-alert>
 
-    <div class="table" style="height: 100vh">
+    <div class="table" style="height: 100vh" id="report">
       
       <v-data-table
         :headers="headers"
@@ -49,107 +50,17 @@
       >
         <template v-slot:top>
           <v-toolbar flat>
-            <v-toolbar-title>Manage Courses</v-toolbar-title>
+            <v-toolbar-title>Course Report</v-toolbar-title>
             <v-divider class="mx-4" inset vertical></v-divider>
             <v-spacer></v-spacer>
-            
-            
-             <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-           
-            ></v-text-field>
              <v-spacer></v-spacer>
-            <template>
-              <v-row align="center" justify="space-around">
-                <v-btn to="/addcourse" color="primary">Add Course</v-btn>
-              </v-row>
-            </template>
 
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5">Delete</v-card-title>
-                <v-card-title class="text-h10">
-                  Are you sure you want to delete this course ?</v-card-title
-                >
-                <v-spacer></v-spacer>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete"
-                    >Cancel</v-btn
-                  >
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="deleteItemConfirm(id)"
-                    >OK</v-btn
-                  >
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-
-            <v-dialog
-              v-if="currentCourse"
-              v-model="editdialog"
-              max-width="500px"
-            >
-              <v-card>
-                <v-card-title>
-                  <span class="text-h5">Edit Course</span>
-                </v-card-title>
-
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-text-field
-                        v-model="currentCourse.name"
-                        label="Name"
-                      ></v-text-field> </v-row
-                    ><v-row>
-                      <v-text-field
-                        v-model="currentCourse.faculty"
-                        label="Faculty"
-                      ></v-text-field> </v-row
-                    ><v-row>
-                      <v-text-field
-                        label="LECTURER"
-                        v-model="currentCourse.lecturer"
-                      >
-                      </v-text-field> </v-row
-                    ><v-row>
-                      <v-text-field
-                        v-model="currentCourse.stdcont"
-                        label="ENROLLMENTS"
-                      ></v-text-field>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="close">
-                    Cencel
-                  </v-btn>
-
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click=" updateCourse(currentCourse)"
-                    >Save</v-btn
-                  >
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+    
           </v-toolbar>
         </template>
 
         <template v-slot:[`item.actions`]="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item.id)"
-            >mdi-pencil</v-icon
-          >
-          <v-icon small @click="deleteItem(item.id)">mdi-delete</v-icon>
+        <a></a>
         </template>
       </v-data-table>
     </div>
@@ -177,12 +88,12 @@ export default {
       message: "",
       headers: [
         // { text: "ID", align: "start", sortable: false, value: "id" },
-        { text: "ID ", value: "no" },
+        { text: "ID ", value: "id" },
         { text: "NAME", value: "name" },
         { text: "FACULTY", value: "faculty" },
         { text: "LECTURER", value: "lecturer" },
          { text: "ENROLLMENTS", value: "stdcont" },
-        { text: "ACTIONS", value: "actions", sortable: false },
+
       ],
     };
   },
@@ -225,7 +136,6 @@ export default {
     getDisplayCourses(course) {
       return {
         id: course._id,
-        no:course.no,
         name: course.name,
         faculty: course.faculty,
         lecturer: course.lecturer,
@@ -257,6 +167,7 @@ export default {
      updateCourse(currentCourse) {
       var id = currentCourse._id;
       var data = {
+       
         name: currentCourse.name,
         faculty: currentCourse.faculty,
         lecturer: currentCourse.lecturer,
@@ -314,6 +225,14 @@ export default {
     alartDisplay(massage) {
       return massage;
     },
+    downloadPDF(){
+        const element = document.getElementById("report");
+        html2pdf().from(element).save();},
+
+
+
+
+
   },
   watch: {
     editdialog(val) {
